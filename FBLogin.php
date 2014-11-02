c1631f963cc4b07de03958d193d22d1840d8480b
Matthew Parangot <mjparangot@gmail.com>
2014-11-02 15:33:25 -0500

    Reworking fb login

--- FBLogin.php
+++ FBLogin.php
@@ -12,6 +12,7 @@
 </head>
 
 <body>
+  <script type="text/javascript" src="js/fb.js"></script>
   
   <div id="main">
     <h1>You're ready to use Parse!</h1>
@@ -29,61 +30,19 @@
 
     <div style="display:none" class="success">
       <p>We've also just created your first object using the following code:</p>
-      
         <code>
-          var TestObject = Parse.Object.extend("TestObject");<br/>
+          <!--var TestObject = Parse.Object.extend("TestObject");<br/>
           var testObject = new TestObject();<br/>
-          testObject.save({foo: "bar"});
+          testObject.save({foo: "bar"});-->
         </code>
-        
     </div>
   </div>
 
+  <!--  
   <script type="text/javascript">
     Parse.initialize("6B6ut2PB7V6850Lb9b96txrM9BU7iWCpEBuoyRjH", "tbPvSvs4uCPn35NMauQSA1TTqsg3EfU2oiLn2rPm");
-
-	window.fbAsyncInit = function() {
-    Parse.FacebookUtils.init({ // this line replaces FB.init({
-      appId      : '507863555984054', // Facebook App ID
-      //status     : true, // check Facebook Login status
-      version    : 'v1.0',
-      cookie     : true, // enable cookies to allow Parse to access the session
-      xfbml      : true
-    });
- 
-      // Run code after the Facebook SDK is loaded.
-      Parse.FacebookUtils.logIn(null, {
-  		success: function(user) {  	
-     		 alert("User logged in through Facebook! access token:");
-     		 alert(Parse.User.current().get('username'));
-    		}
-  		},
- 		   error: function(user, error) {
-   			 alert("User cancelled the Facebook login or did not fully authorize.");
-  		}
-	});
-  	};
-  	
- 	(function(d, s, id){
-    	var js, fjs = d.getElementsByTagName(s)[0];
-    	if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/all.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-  
-    
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-      testObject.save({foo: "bar"}, {
-      success: function(object) {
-        $(".success").show();
-      },
-      error: function(model, error) {
-        $(".error").show();
-      }
-    });
   </script>
-</body>
+  -->
+  </body>
 
 </html>
---
+++ js/fb.js
@@ -0,0 +1,31 @@
+Parse.initialize("6B6ut2PB7V6850Lb9b96txrM9BU7iWCpEBuoyRjH", "tbPvSvs4uCPn35NMauQSA1TTqsg3EfU2oiLn2rPm");
+
+window.fbAsyncInit = function() {
+  Parse.FacebookUtils.init({ // this line replaces FB.init({
+    appId      : '507863555984054',
+    status     : true, // check Facebook Login status
+    cookie     : true, // enable cookies to allow Parse to access the session
+    xfbml      : true
+  });
+
+  Parse.FacebookUtils.logIn(null, {
+    success: function(user) {
+      if (!user.existed()) {
+        alert("User signed up and logged in through Facebook!");
+      } else {
+        alert("User logged in through Facebook!");
+      }
+    },
+    error: function(user, error) {
+      alert("User cancelled the Facebook login or did not fully authorize.");
+    }
+  });
+};
+
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
