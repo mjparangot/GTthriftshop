0a64ddc4af2175763be13a3c7c6fdff366fa3ea8
Matthew Parangot <mjparangot@lawn-143-215-206-119.lawn.gatech.edu>
2014-11-30 18:08:09 -0500

    Front end changes

--- index.php
+++ index.php
@@ -54,10 +54,10 @@
           <li class="has-form">
             <div class="row collapse">
               <form id="searchpanel" action="" method="get">
-      				  <div class="large-8 small-9 columns">
+      				  <div class="large-12 small-9 columns">
                   <input id="searchbar" name="search" type="text" placeholder="Find Stuff">
       				  </div>
-      				  <div class="large-4 small-3 columns">
+      				  <div class="small-3 columns">
                   <input type="submit" value="Search" id="searchbutton" href="#" class="alert button expand"/>
       				  </div>
               </form>
