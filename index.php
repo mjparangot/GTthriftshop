2c90387165184546154438b86b1feb072378982e
Stefan Koshy <noreply@skoshy.com>
2014-11-30 16:21:43 -0500

    Bugfix

--- index.php
+++ index.php
@@ -15,9 +15,10 @@
           <!-- If they are not logged into Facebook, show login button -->
           <!-- If they are logged into Facebook, our webapp accesses their fb automatically -->
           <?php
-		  if (false)
-          	<li class="active"><a href="#" onclick="loginWithFacebook()">Login with Facebook</a></li>
-          ?>
+		  if (false) {
+		  ?>
+			<li class="active"><a href="#" onclick="loginWithFacebook()">Login with Facebook</a></li>
+          <? } ?>
           <li class="divider"></li>
           <li class="active make-post"><a href="#" data-reveal-id="post-modal">Make a Post</a></li>
           <!-- Make a post modal -->
