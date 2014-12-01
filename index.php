382431e2993ad50e891c79f4674c79a15221543d
kcheng1115 <kcheng37@gatech.edu>
2014-11-30 23:45:04 -0500

    fb send button

--- index.php
+++ index.php
@@ -58,13 +58,15 @@
             <p>You can attach images in Facebook before you post to the group.</p>
             <a href="#" class="button">Add Another Item</a>
             <!-- <a href="#" class="button">Submit</a> -->
-            <a href="#" id="submit-button" class="button active make-post hidden-element" data-reveal-id="fb-send-modal">Submit</a>
+            <a href="#" class="button" data-reveal-id="fb-send-modal">Submit</a>
             
             <!-- FB Send Button Popup -->
             <div id="fb-send-modal" class="reveal-modal" data-reveal>
-            <p class="lead">Would you like to share your post with the GT Thrift Shop Group?</p>
-            <div class="fb-send" data-href="http://gt-thrift-shop-test.herokuapp.com/#" data-colorscheme="light"></div>
-            </div>
+            <h2>Awesome. I have it.</h2>
+  <p class="lead">Your couch.  It is mine.</p>
+  <p>I'm a cool paragraph that lives inside of an even cooler modal. Wins!</p>
+  <a class="close-reveal-modal">&#215;</a>
+</div>
             
             <a class="close-reveal-modal">&#215;</a>
           </div>
