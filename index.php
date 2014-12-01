d7c88f44b77eb459aa4912902ff642922c8fc056
Matthew Parangot <mjparangot@lawn-143-215-206-119.lawn.gatech.edu>
2014-12-01 00:30:32 -0500

    Front end changes

--- index.php
+++ index.php
@@ -153,7 +153,7 @@
               if ($item['price'] != -1)
                 echo '<li class="price">$'.$item['price'].'</li>';
               else
-                echo '<a href="<?=$item["postlink"]?>" target="_blank" class="button radius round">View Post</a>'
+                echo '<a href="<?=$item['postlink']?>" target="_blank" class="button radius round">View Post</a>'
             ?>
   				</ul>
   			</li>
