18c362a15e5d797323bf37d9924bdeae6623375b
Matthew Parangot <mjparangot@lawn-143-215-206-119.lawn.gatech.edu>
2014-11-30 23:44:27 -0500

    Front end changes

--- index.php
+++ index.php
@@ -116,7 +116,7 @@
         						if (strpos($pic, '://') === FALSE)
         							$url = "https://graph.facebook.com/".$pic."/picture";
         						else
-        							$url = "http://lorempixel.com/g/800/600/abstract/No%8Fimage%8Favailable";
+        							$url = "http://lorempixel.com/g/800/600/abstract/No%20image%20available";
         						?>
         							<li class="item-image">
                         <img src="<?= $url ?>">
