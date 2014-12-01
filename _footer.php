c639dcc7d964a8f9bfc8f2a097822841c559d54f
kcheng1115 <kcheng37@gatech.edu>
2014-11-30 21:20:01 -0500

    fb login

--- _footer.php
+++ _footer.php
@@ -106,7 +106,7 @@
   		  	alert("logging out");
   		  	alert("current "+Parse.User.current());
   		  	FB.logout(function(response) {
-  				console.log(user is now logged out);
+  				//console.log(user is now logged out);
 			});
   		};
   	
