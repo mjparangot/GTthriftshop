cb62effdc096d8e2d3f4d08c7ee491c4ca3d91bb
Matthew Parangot <mjparangot@lawn-143-215-206-119.lawn.gatech.edu>
2014-11-30 22:29:00 -0500

    Front end changes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <nav class="top-bar" data-topbar role="navigation">
       <ul class="title-area">
         <li class="name">
-          <h1><a href="gt-thrift-shop-test.herokuapp.com">GT Thrift Shop</a></h1>
+          <h1><a href="http://gt-thrift-shop-test.herokuapp.com">GT Thrift Shop</a></h1>
         </li>
          <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
         <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
@@ -15,10 +15,10 @@
           <!-- If they are not logged into Facebook, show login button -->
           <!-- If they are logged into Facebook, our webapp accesses their fb automatically -->
           <a href="#" id="fb-login" class="button active" onclick="loginWithFacebook()">Login with Facebook</a>
-  <a href="#" id="fb-logout" class="button active hidden-element" onclick="logoutWithFacebook()">Logout</a>
-  <a href="#" id="make-post-button" class="button active make-post hidden-element" data-reveal-id="post-modal">Make a Post</a>
-  <a href="#" id="make-post-button-disabled" class="button transparent-element" onclick="alert('You must be logged in to make a post!');">Make a Post</a>
-<!-- Make a post modal -->
+          <a href="#" id="fb-logout" class="button active hidden-element" onclick="logoutWithFacebook()">Logout</a>
+          <a href="#" id="make-post-button" class="button active make-post hidden-element" data-reveal-id="post-modal">Make a Post</a>
+          <a href="#" id="make-post-button-disabled" class="button transparent-element" onclick="alert('You must be logged in to make a post!');">Make a Post</a>
+          <!-- Make a post modal -->
           <div id="post-modal" class="reveal-modal" data-reveal>
             <p class="lead">Use this form to create a post on the GT Thrift Shop Facebook page.</p>
             <p>This form helps us create a standard structure for posts which makes it easier to populate this site.</p>
@@ -109,6 +109,7 @@
         						?>
         							<li class="item-image">
                         <img src="<?= $url ?>">
+                          <a href="<?=$item['postlink']?>" class="button view-post-btn">View Post</a>
                           <div class="dim-overlay"></div>
                         </img>
                       </li>
